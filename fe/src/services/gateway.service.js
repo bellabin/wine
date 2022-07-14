fc1865e61a2a15b7195faf6db664aa92b0cfe1b3
admin <admin@admins-MacBook-Pro.local>
2022-07-14 22:36:35 +0700

    inital fe pages

---
+++ fe/src/services/gateway.service.js
@@ -0,0 +1,17 @@
+import Axios from "axios";
+import { getTokenLocal, setTokenLocal } from "../../utils/Common";
+
+const HttpService = Axios.create({
+  baseURL: "http://127.0.0.1:3000",
+  timeout: 30000,
+  withCredentials: true,
+  headers: {
+    Authorization: "Bearer " + getTokenLocal(),
+  },
+});
+
+export const SetToken = (token) => {
+  HttpService.defaults.headers.common["Authorization"] = "Bearer " + token;
+  setTokenLocal(token);
+};
+export default HttpService;
