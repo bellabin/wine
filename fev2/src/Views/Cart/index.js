84036fb1962527135a241ca5637378db58d8e4d1
severus <truongquynh2525@gmail.com>
2022-08-03 21:27:17 +0700

    add paypal button

--- fev2/src/Views/Cart/index.js
+++ fev2/src/Views/Cart/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
+import Paypal from '../../components/Paypal'
 import Body from "./Body"
 
 export default class Cart extends Component {
     render() {
         return (
             <Layout>
+                <Paypal/>
                 <Body/>
             </Layout>
         )
---
+++ fev2/src/components/Paypal.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { PayPalButton } from 'react-paypal-button-v2';
+
+export default class Paypal extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: 100
+    }
+  }
+
+  componentDidMount() {
+    // Call api to get total of the order
+  }
+
+  // function to save the order
+  handleSaveOrder() {
+    // call api to save order here
+    console.log("save order")
+  }
+
+  render() {
+    return (
+      <PayPalButton
+        amount="0.01"
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                currency_code: "USD",
+                value: this.state.total
+              }
+            }],
+          });
+        }}
+        onSuccess={(details, data) => this.handleSaveOrder(details, data)}
+      />
+    )
+  }
+}
