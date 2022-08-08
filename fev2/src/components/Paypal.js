de5d092d342a3bbab00a5518f6db723db1f70c1d
tannd <tannd@itsj-group.com.vn>
2022-08-09 00:35:01 +0700

    push Paypal component

--- fev2/src/components/Paypal.js
+++ fev2/src/components/Paypal.js
@@ -1,44 +1,101 @@
-import React, { Component } from 'react'
-import { PayPalButton } from 'react-paypal-button-v2';
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer
+} from "@paypal/react-paypal-js";
+import { PayPalScriptOptions, PayPalButtonsComponentProps} from "@paypal/paypal-js";
 
-export default class Paypal extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      total: 100
-    }
-  }
-
-  componentDidMount() {
-    // Call api to get total of the order
-  }
-
-  // function to save the order
-  handleSaveOrder() {
-    // call api to save order here
-    console.log("save order")
-  }
-
-  render() {
-    return (
-      <>
-        <PayPalButton
-        amount="0.01"
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [{
-              amount: {
-                currency_code: "USD",
-                value: this.state.total
+const paypalScriptOptions: PayPalScriptOptions = {
+  "client-id":
+      "AaaFcJt0sRvpH2wvW0ZvhaD2myM0PpZyEAM4vzamKXoWRAOyuIgkNXXKOKRJpAa4ACvjTiuWwF714d3_",
+  currency: "USD"
+};
+function Button() {
+  /**
+   * usePayPalScriptReducer use within PayPalScriptProvider
+   * isPending: not finished loading(default state)
+   * isResolved: successfully loaded
+   * isRejected: failed to load
+   */
+  const [{ isPending }] = usePayPalScriptReducer();
+  const paypalbuttonTransactionProps: PayPalButtonsComponentProps = {
+    style: { layout: "vertical" },
+    createOrder(data, actions) { // ham nay goi khi bam vao nut pop up Paypal
+      return actions.order.create({
+        purchase_units: [
+          {
+            amount: {
+              value: "25", // value phai = tong tat cả value trong breakdown
+              breakdown: {
+                item_total: { // item_total phai = unit_amount * quantity trong tat ca item
+                  currency_code: "USD",
+                  value: "22"
+                },
+                shipping: {
+                  currency_code: "USD",
+                  value: "3"
+                }
               }
-            }],
-          });
-        }}
-        onSuccess={(details, data) => this.handleSaveOrder(details, data)}
-      />
-        
+            },
+            items: [ // sau nay thay bang list product khi thanh toan
+              {
+                name: "TEST_NAME",
+                unit_amount: {
+                  currency_code: "USD",
+                  value: "6"
+                },
+                quantity: "1",
+              },
+              {
+                name: "TEST_NAME_1",
+                unit_amount: {
+                  currency_code: "USD",
+                  value: "8"
+                },
+                quantity: "2",
+              }
+            ],
+          }
+        ]
+      });
+    },
+    onApprove(data, actions) { // ham nay goi khi thanh toan thanh cong
+      /**
+       * data: {
+       *   orderID: string;
+       *   payerID: string;
+       *   paymentID: string | null;
+       *   billingToken: string | null;
+       *   facilitatorAccesstoken: string;
+       * }
+       */
+      return actions.order.capture({}).then((details) => {
+        alert(
+            "Transaction completed by" +
+            (details?.payer.name.given_name ?? "No details")
+        );
 
+        alert("Data details: " + JSON.stringify(data, null, 2));
+      });
+    },
+    onError(err: Record<string, unknown>) { // ham nay goi khi thanh toan loi
+      console.log('error paypal', err.toString())
+      alert("The Transaction has error!!!!")
+    }
+  };
+  return (
+      <>
+        {isPending ? <h2>Load Smart Payment Button...</h2> : null}
+        <PayPalButtons {...paypalbuttonTransactionProps} />
       </>
-    )
-  }
+  );
+}
+export default function Paypal() {
+  return (
+      <div style={{textAlign: "center"}}>
+        <PayPalScriptProvider options={paypalScriptOptions}>
+          <Button />
+        </PayPalScriptProvider>
+      </div>
+  );
 }
