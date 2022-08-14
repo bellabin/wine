708cea45ca81accdb8a160a675df1d713026622e
severus <truongquynh2525@gmail.com>
2022-08-14 07:57:54 +0700

    create customer

--- be/src/modules/customers/customer.module.ts
+++ be/src/modules/customers/customer.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { StaffModule } from "../staffs/staff.module";
 import { CustomerController } from "./customer.controller";
 import { CustomerService } from "./customer.service";
 import { Customer } from "./entities/customer.entity";
 
-
 @Module({
-    imports: [TypeOrmModule.forFeature([Customer])],
-    controllers: [CustomerController],
-    providers: [CustomerService],
-    exports:[CustomerService]
+  imports: [TypeOrmModule.forFeature([Customer]), StaffModule],
+  controllers: [CustomerController],
+  providers: [CustomerService],
+  exports: [CustomerService],
 })
 export class CustomerModule {}
--- be/src/modules/customers/customer.service.ts
+++ be/src/modules/customers/customer.service.ts
@@ -1,58 +1,77 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { hashSync } from "bcrypt";
 import { Repository } from "typeorm";
+import { StaffService } from "../staffs/staff.service";
 import { CreateCustomerDto } from "./dto/create-customer.dto";
 import { UpdateCustomerDto } from "./dto/update-customer.dto";
 import { Customer } from "./entities/customer.entity";
 
 @Injectable()
 export class CustomerService {
-    constructor(
-        @InjectRepository(Customer) private customerRepo: Repository<Customer>
-        ) {}
+  constructor(
+    @InjectRepository(Customer) private customerRepo: Repository<Customer>,
+    private staffService: StaffService
+  ) {}
 
-    findAll() {
-        return this.customerRepo.find({
-            relations: ['role', 'phieudats'],
-          })
+  findAll() {
+    return this.customerRepo.find({
+      relations: ["role", "phieudats"],
+    });
+  }
+
+  findById(MAKH: string) {
+    return this.customerRepo.findOne({
+      where: { MAKH: MAKH },
+      relations: ["role", "phieudats"],
+    });
+  }
+
+  findByUsername(USERNAME: string) {
+    return this.customerRepo
+      .createQueryBuilder("customers")
+      .where("customers.USERNAME = :USERNAME", { USERNAME })
+      .getOne();
+  }
+
+  async create(payload: CreateCustomerDto) {
+    try {
+      const { USERNAME, PASSWORD } = payload;
+
+      const staff = await this.staffService.findByUsername(USERNAME);
+
+      if (staff) throw new ConflictException("Username is existing");
+
+      const hashedPassword = hashSync(PASSWORD, 10);
+      payload.PASSWORD = hashedPassword;
+
+      const customer = this.customerRepo.create(payload);
+
+      await this.customerRepo.save(customer);
+
+      return customer;
+    } catch (error) {
+      throw new ConflictException(error.response);
     }
-    
-    findById(MAKH : string) {
-        return this.customerRepo.findOne({
-            where: { MAKH: MAKH  },
-            relations: ['role', 'phieudats'],
-          })
-    }
+  }
 
-    findByUsername(USERNAME: string) {
-        return this.customerRepo
-            .createQueryBuilder('customers')
-            .where('customers.USERNAME = :USERNAME', {USERNAME})
-            .getOne()
-    
-    }
+  async update(MAKH: string, body: UpdateCustomerDto) {
+    const customer = await this.findById(MAKH);
 
-   async create (payload:CreateCustomerDto)  {
-    const customer = this.customerRepo.create(payload)
+    if (!customer) throw new NotFoundException("Cus is not exist");
 
-    await this.customerRepo.save(customer)
+    return this.customerRepo.update(MAKH, body);
+  }
 
-    return customer
-   }
+  async delete(MAKH: string) {
+    const customer = await this.findById(MAKH);
 
-   async update (MAKH: string, body: UpdateCustomerDto) {
-    const customer = await this.findById(MAKH)
+    if (!customer) throw new NotFoundException("Customer is not exist");
 
-    if(!customer) throw new NotFoundException ('Cus is not exist')
-
-    return this.customerRepo.update(MAKH, body)
-   }
-
-   async delete (MAKH: string) {
-    const customer = await this.findById(MAKH)
-
-    if(!customer) throw new NotFoundException('Customer is not exist')
-
-    return this.customerRepo.remove(customer)
-   }
+    return this.customerRepo.remove(customer);
+  }
 }
